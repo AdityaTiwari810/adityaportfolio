c74562ae1b69126583f4cf0c0cac13f89d76901b
unknown <adityatiwari.aaa@gmail.com>
2021-12-18 00:15:56 +0530

    first

--- mail.php
+++ mail.php
@@ -1,11 +1,16 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "adityatiwari.aaa@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "adityatiwari.aaa@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@adiportfolio.com" . "\r\n" .
+"CC: adityatiwari.sdl@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
 header("Location:thankyou.html");
 ?>
