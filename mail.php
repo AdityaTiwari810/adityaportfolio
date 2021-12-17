513d26488efab24ca49dc29b744a017ab884204e
unknown <adityatiwari.aaa@gmail.com>
2021-12-17 23:48:20 +0530

    first

--- mail.php
+++ mail.php
@@ -1,28 +1,10 @@
-<?php 
-
-    if(isset($_POST['btn-send']))
-    {
-       $UserName = $_POST['name'];
-       $Email = $_POST['email'];
-       $Subject = $_POST['subject'];
-       $Msg = $_POST['message'];
-
-       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
-       {
-           header('location:index.html?error');
-       }
-       else
-       {
-           $to = "adityatiwari.aaa@gmail.com";
-
-           if(mail($to,$Subject,$Msg,$Email))
-           {
-               header("location:thankyou.html");
-           }
-       }
-    }
-    else
-    {
-        header("location:thankyou.html");
-    }
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "adityatiwari.aaa@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
