3ff70bf370c56974ed69dcab370231991c508c4f
unknown <adityatiwari.aaa@gmail.com>
2021-12-17 18:21:26 +0530

    first commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "adityatiwari.sdl@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@aditya.com" . "\r\n" .
+"CC: adityatiwari.aaa@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
+
