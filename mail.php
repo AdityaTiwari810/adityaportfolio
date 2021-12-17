a697ce34fc52739926da2a93f71b1210b5aae2df
unknown <adityatiwari.aaa@gmail.com>
2021-12-17 23:20:58 +0530

    first

--- mail.php
+++ mail.php
@@ -1,17 +1,28 @@
-<?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "adityatiwari.sdl@gmail.com";
-$subject = "Mail From Portfolio website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@aditiwari.com" . "\r\n" .
-"CC: adityatiwari.aaa@gmail.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
-?>
+<?php 
 
+    if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['name'];
+       $Email = $_POST['email'];
+       $Subject = $_POST['subject'];
+       $Msg = $_POST['message'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index.html?error');
+       }
+       else
+       {
+           $to = "adityatiwari.aaa@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:thankyou.html");
+           }
+       }
+    }
+    else
+    {
+        header("location:thankyou.html");
+    }
+?>
