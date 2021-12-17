e3a763bdadab1b0e8efd282a649c8e9ab522f96a
unknown <adityatiwari.aaa@gmail.com>
2021-12-17 21:25:30 +0530

    first

--- mail.php
+++ mail.php
@@ -4,9 +4,9 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "adityatiwari.sdl@gmail.com";
-$subject = "Mail From website";
+$subject = "Mail From Portfolio website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: 1805275@kiit.ac.in" . "\r\n" .
+$headers = "From: noreply@aditiwari.com" . "\r\n" .
 "CC: adityatiwari.aaa@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
